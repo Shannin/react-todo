f260bc7fb6dbdf4d269b60d6fc1ab3fb5a919474
Shannin Ciprich <shanninc@gmail.com>
2015-07-16 19:58:21 -0700

    add new item button, structure like real app

--- app/components/ToDoList.js
+++ app/components/ToDoList.js
@@ -5,38 +5,60 @@ require('../stylesheets/ToDoList.scss')
 class ToDoListItemRow extends React.Component {
   render() {
     return (
-      <div className="todo-list__item-container__item-row">
-        {this.props.content}
-      </div>
+      <li className="todo-list__item-container__item-row">
+        {this.props.title}
+      </li>
     );
   }
 }
 
 class ToDoListHeader extends React.Component {
+
   render() {
     return (
       <div className="todo-list__header">
         <h3>{this.props.title}</h3>
+        <div className="todo-list__header__new-item-button"
+            onClick={this.props.onCreate.bind(this)}
+        >+</div>
       </div>
     );
   }
 }
 
 class ToDoList extends React.Component {
+
+  newItem() {
+    console.log("clicked!")
+  }
+
   render() {
+    var items = this.props.items.map(function (item) {
+      return (<ToDoListItemRow title={item.title} />)
+    });
+
     return (
       <div className="todo-list">
-        <ToDoListHeader title="some title" />
-        <div className="todo-list__item-container">
-          <ToDoListItemRow content="this would be the content" />
-          <ToDoListItemRow content="this would be the content" />
-          <ToDoListItemRow content="this would be the content" />
-          <ToDoListItemRow content="this would be the content" />
-          <ToDoListItemRow content="this would be the content" />
-        </div>
+        <ToDoListHeader title="some title"
+          onCreate={this.newItem} />
+        <ul className="todo-list__item-container">
+          {items}
+        </ul>
       </div>
     );
   }
 }
+
+ToDoList.defaultProps= {
+    items: [{
+      title: 'title 1'
+    },{
+      title: 'title 2'
+    },{
+      title: 'title 3'
+    },{
+      title: 'title 4'
+    }]
+  }
 
 export default ToDoList;
